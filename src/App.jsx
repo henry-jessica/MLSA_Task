284c13d2a3a9a6e54ab2dcfa3d9a023f0823ef5d
jessica <jessicahenry2604@gmail.com>
2023-10-05 19:31:21 +0100

    information update

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jessica Henry",
+  title: "Web Developer and Content Creator",
+  email: "Jessica.Henry@studentambassadors.com",
+  gitHub: "henry-jessica",
+  instagram: "",
+  linkedIn: "jessica-henry2604",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
