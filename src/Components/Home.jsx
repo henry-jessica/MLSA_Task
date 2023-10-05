286dda3398edbb1059cb03119ed19f9cf4e59664
jessica <jessicahenry2604@gmail.com>
2023-10-05 19:42:06 +0100

    add image description

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,7 +22,7 @@ import PropTypes from "prop-types";
  */
 import image from "../images/pexels-vera-cires-41227.jpeg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Computer on top of table";
 
 const Home = ({ name, title }) => {
   return (
